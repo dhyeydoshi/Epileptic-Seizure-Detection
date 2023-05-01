326ef2952a912a048e2d01f35025039628117e11
Dhyey Doshi <curiousmind010@gmail.com>
2023-05-01 17:46:17 -0400

    bump v1.2.4

--- epileptic_seizure_detection/settings.py
+++ epileptic_seizure_detection/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d+u@2_13y%u5(kno0&s$hmkys#n&pga2rb2qzn(8+bo$634)8$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
