93f8f26d4068c54c232e47aaf73d4b281d30e6dd
Dhyey Doshi <62507566+dhyeydoshi@users.noreply.github.com>
2024-04-24 00:37:09 -0400

    Update settings.py

--- epileptic_seizure_detection/settings.py
+++ epileptic_seizure_detection/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static_root/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static/",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
