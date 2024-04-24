78d9f7634fb0743700e22a4eefc69e4a97e177a3
Dhyey Doshi <curiousmind010@gmail.com>
2024-04-24 01:07:47 -0400

    bump v1.3.5

--- epileptic_seizure_detection/settings.py
+++ epileptic_seizure_detection/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,7 @@ STATIC_ROOT = BASE_DIR / 'static_root/'
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
